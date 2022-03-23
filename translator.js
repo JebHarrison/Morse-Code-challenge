81f56dda858a565fd13761db1b7c66cf6943f45f
James Harrison <jamesebharrison@hotmail.com>
2022-03-23 16:24:13 +0000

    changed format of js and added file

---
+++ translator.js
@@ -0,0 +1,39 @@
+let binary = {
+    a: "10",
+    b: "0111",
+    c: "0101",
+    d: "011",
+    e: "1",
+    f: "1101",
+    g:"001",
+    h: "1111",
+    i: "11",
+    j: "1000",
+    k: "010",
+    l: "1011",
+    m: "00",
+    n: "01",
+    o: "000",
+    p: "1001",
+    q: "0010",
+    r: "101",
+    s: "111",
+    t: "0",
+    u: "110",
+    v: "1110",
+    w: "100",
+    x: "0110", 
+    y: "0100", 
+    z: "0011",
+}
+
+export class Translator {
+    constructor(input, binary) {
+        this.input = input;
+        this.binary = binary;
+    }
+
+    translateInput() {
+
+    }
+}
